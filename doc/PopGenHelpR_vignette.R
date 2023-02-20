0e27b61307fa989cdf8e502a253cbf1fff132c55
kfarleigh <farleik@miamioh.edu>
2023-02-20 15:10:59 -0500

    Add files via upload

--- doc/PopGenHelpR_vignette.R
+++ doc/PopGenHelpR_vignette.R
@@ -7,6 +7,9 @@ knitr::opts_chunk$set(
 ## ----setup--------------------------------------------------------------------
 # Load the package
 library(PopGenHelpR)
+# Install rnaturalearthhires if you need it
+# install.packages("devtools")
+# devtools::install_github("ropensci/rnaturalearthhires")
 
 ## ----load data----------------------------------------------------------------
 data("Fst_dat")
@@ -20,7 +23,7 @@ Loc <- Q_dat[[2]]
 
 # Now we will generate both population and individual plots by seeting plot.type to 'all'. If you wanted, you could only generate individual or population plots by setting plot.type to "individual" and "population", respectively.
 Test_all <- Plot_ancestry(anc.mat = Qmat, pops = Loc, K = 5,
-plot.type = 'all', col = c('red', 'orange', 'pink', 'purple', 'blue'), countries = c("united states of america", "mexico"), Lat_buffer = 1, Long_buffer = 1)
+plot.type = 'all', col = c('#d73027', '#f46d43', '#e0f3f8', '#74add1', '#313695'), countries = c("united states of america", "mexico"), Lat_buffer = 1, Long_buffer = 1)
 
 ## ----Plot Ancestry Maps-------------------------------------------------------
 Test_all$`Individual Map`
@@ -39,13 +42,13 @@ Fstat_hmap
 # Closest Neighbor
 Fst_map <- Dif_Stats_Map(dat = Fst, pops = Loc, neighbors = 1,
                      countries = c('united states of america', 'mexico'),
-                     col = c('#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'),
+                     col = c('#FFFF00','#FFC000','#FFA500','#e31a1c','#800026'),
                      Lat_buffer = 1, Long_buffer = 1)
 Fst_map$`Differentiation Map`
 
 
 Fst_map2 <- Dif_Stats_Map(dat = Fst, pops = Loc, neighbors = c('East_West', 'East_South', 'South_West'), countries = c('united states of america', 'mexico'),
-                     col = c('#fd8d3c','#fc4e2a','#e31a1c','#bd0026','#800026'),
+                     col = c('#FFFF00','#FFC000','#FFA500','#e31a1c','#800026'),
                      Lat_buffer = 1, Long_buffer = 1)
 Fst_map2$`Differentiation Map`
 
