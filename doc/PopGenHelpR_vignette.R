899363c6ce19fbb71a38e4ebbd8eec91e245908f
Keaka Farleigh <farleik@miamioh.edu>
2023-09-28 20:49:55 -0400

    Add files via upload

--- doc/PopGenHelpR_vignette.R
+++ doc/PopGenHelpR_vignette.R
@@ -7,27 +7,28 @@ knitr::opts_chunk$set(
 ## ----setup--------------------------------------------------------------------
 # Load the package
 library(PopGenHelpR)
-# Install rnaturalearthhires if you need it
-# install.packages("devtools")
-# devtools::install_github("ropensci/rnaturalearthhires")
 
 ## ----load data----------------------------------------------------------------
 data("Fst_dat")
 data("Het_dat")
 data("Q_dat")
+data("HornedLizard_Pop")
+data("HornedLizard_VCF")
 
 ## ----Plot Ancestry------------------------------------------------------------
 # First, we seperate the list elements into two seperate objects. The q-matrix (Qmat) and the locality information for each individual (Loc).
 Qmat <- Q_dat[[1]]
 Loc <- Q_dat[[2]]
 
-# Now we will generate both population and individual plots by seeting plot.type to 'all'. If you wanted, you could only generate individual or population plots by setting plot.type to "individual" and "population", respectively.
+# Now we will generate both population and individual plots by setting plot.type to 'all'. If you wanted, you could only generate individual or population plots by setting plot.type to "individual" and "population", respectively.
 Test_all <- Plot_ancestry(anc.mat = Qmat, pops = Loc, K = 5,
-plot.type = 'all', col = c('#d73027', '#f46d43', '#e0f3f8', '#74add1', '#313695'), countries = c("united states of america", "mexico"), Lat_buffer = 1, Long_buffer = 1)
+plot.type = 'all', col = c('#d73027', '#f46d43', '#e0f3f8', '#74add1', '#313695'), Lat_buffer = 1, Long_buffer = 1)
 
-## ----Plot Ancestry Maps-------------------------------------------------------
-Test_all$`Individual Map`
-Test_all$`Population Map`
+
+Test_all$`Individual Ancestry Plot`
+
+
+Test_all$`Population Ancestry Plot`
 
 ## ----Differentiation----------------------------------------------------------
 # Isolate our fst matrix and locality information
@@ -41,13 +42,11 @@ Fstat_hmap
 ## ----Differentiation map------------------------------------------------------
 # Closest Neighbor
 Fst_map <- Dif_Stats_Map(dat = Fst, pops = Loc, neighbors = 1,
-                     countries = c('united states of america', 'mexico'),
                      col = c('#FFFF00','#FFC000','#FFA500','#e31a1c','#800026'),
                      Lat_buffer = 1, Long_buffer = 1)
 Fst_map$`Differentiation Map`
 
-
-Fst_map2 <- Dif_Stats_Map(dat = Fst, pops = Loc, neighbors = c('East_West', 'East_South', 'South_West'), countries = c('united states of america', 'mexico'),
+Fst_map2 <- Dif_Stats_Map(dat = Fst, pops = Loc, neighbors = c('East_West', 'East_South', 'South_West'),
                      col = c('#FFFF00','#FFC000','#FFA500','#e31a1c','#800026'),
                      Lat_buffer = 1, Long_buffer = 1)
 Fst_map2$`Differentiation Map`
@@ -55,7 +54,7 @@ Fst_map2$`Differentiation Map`
 ## ----Diversity----------------------------------------------------------------
 # Similar to our heat map, we use the statistic argument to label our figures and any output raster you create.
 Div_map <- Div_Stats_Map(dat = Het_dat, plot.type = 'point',
-statistic = "Heterozygosity", col = c('blue', 'orange', 'red'), countries = c('united states of america', 'mexico'), Lat_buffer = 1, Long_buffer = 1, prefix = 'Test_het')
+statistic = "Heterozygosity", col = c('blue', 'orange', 'red'), Lat_buffer = 1, Long_buffer = 1, prefix = 'Test_het')
 
 Div_map$`Heterozygosity Map`
 
