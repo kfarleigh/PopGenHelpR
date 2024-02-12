1db6f56b2ffe883728880feb7707a651ae265e99
kfarleigh <farleik@miamioh.edu>
2024-02-12 16:21:45 -0500

    Edited vignette, updated website

--- doc/PopGenHelpR_vignette.R
+++ doc/PopGenHelpR_vignette.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -15,46 +15,86 @@ data("Q_dat")
 data("HornedLizard_Pop")
 data("HornedLizard_VCF")
 
-## ----Plot Ancestry------------------------------------------------------------
-# First, we seperate the list elements into two seperate objects. The q-matrix (Qmat) and the locality information for each individual (Loc).
-Qmat <- Q_dat[[1]]
-Loc <- Q_dat[[2]]
+## ----Ancestry barchart, echo=TRUE, eval=FALSE---------------------------------
+#  # First, we seperate the list elements into two seperate objects. The q-matrix (Qmat) and the locality information for each individual (Loc).
+#  Qmat <- Q_dat[[1]]
+#  Loc <- Q_dat[[2]]
+#  
+#  # Now we will generate both population and individual plots by setting plot.type to 'all'. If you wanted, you could only generate individual or population plots by setting plot.type to "individual" and "population", respectively.
+#  Test_all <- Ancestry_barchart(anc.mat = Qmat, pops = Loc, K = 5,
+#  plot.type = 'all', col = c('#d73027', '#f46d43', '#e0f3f8', '#74add1', '#313695'))
+#  
+#  Test_all$`Individual Ancestry Plot`
 
-# Now we will generate both population and individual plots by setting plot.type to 'all'. If you wanted, you could only generate individual or population plots by setting plot.type to "individual" and "population", respectively.
-Test_all <- Plot_ancestry(anc.mat = Qmat, pops = Loc, K = 5,
-plot.type = 'all', col = c('#d73027', '#f46d43', '#e0f3f8', '#74add1', '#313695'), Lat_buffer = 1, Long_buffer = 1)
+## ----out.width= "600px", out.height= "350px", echo=FALSE, eval=TRUE, fig.align='center'----
+knitr::include_graphics("./img/Anc_barchart1.png")
 
+## ----eval=FALSE---------------------------------------------------------------
+#  Test_all$`Population Ancestry Plot`
 
-Test_all$`Individual Ancestry Plot`
+## ----out.width= "600px", out.height= "350px", echo=FALSE, eval=TRUE, fig.align='center'----
+knitr::include_graphics("./img/Anc_barchart2.png")
 
+## ----Piechart map, echo=TRUE, eval=FALSE, fig.align='center'------------------
+#  # First, we seperate the list elements into two seperate objects. The q-matrix (Qmat) and the locality information for each individual (Loc).
+#  Qmat <- Q_dat[[1]]
+#  Loc <- Q_dat[[2]]
+#  
+#  # Now we will generate both population and individual plots by setting plot.type to 'all'. If you wanted, you could only generate individual or population plots by setting plot.type to "individual" and "population", respectively.
+#  Test_all_piemap <- Piechart_map(anc.mat = Qmat, pops = Loc, K = 5,plot.type = 'all', col = c('#d73027', '#f46d43', '#e0f3f8', '#74add1', '#313695'),
+#                                  Lat_buffer = 1, Long_buffer = 1)
+#  
+#  Test_all_piemap$`Individual Map`
 
-Test_all$`Population Ancestry Plot`
+## ----out.width= "500px", out.height= "750px", echo=FALSE, eval=TRUE, fig.align='center'----
+knitr::include_graphics("./img/Ind_PieMap.png")
 
-## ----Differentiation----------------------------------------------------------
-# Isolate our fst matrix and locality information
-Fst <- Fst_dat[[1]]
-Loc <- Fst_dat[[2]]
-# Plot the heatmap, the statistic argument is used to label the plot.
-Fstat_hmap <- Pairwise_heatmap(dat = Fst, statistic = 'FST')
-# Look at the plot
-Fstat_hmap
+## ----Piechart map2, echo=TRUE, eval=FALSE, fig.align='center'-----------------
+#  Test_all_piemap$`Population Map`
 
-## ----Differentiation map------------------------------------------------------
-# Closest Neighbor
-Fst_map <- Dif_Stats_Map(dat = Fst, pops = Loc, neighbors = 1,
-                     col = c('#FFFF00','#FFC000','#FFA500','#e31a1c','#800026'),
-                     Lat_buffer = 1, Long_buffer = 1)
-Fst_map$`Differentiation Map`
+## ----out.width= "500px", out.height= "750px", echo=FALSE, eval=TRUE, fig.align='center'----
+knitr::include_graphics("./img/Pop_PieMap.png")
 
-Fst_map2 <- Dif_Stats_Map(dat = Fst, pops = Loc, neighbors = c('East_West', 'East_South', 'South_West'),
-                     col = c('#FFFF00','#FFC000','#FFA500','#e31a1c','#800026'),
-                     Lat_buffer = 1, Long_buffer = 1)
-Fst_map2$`Differentiation Map`
+## ----Differentiation, echo = TRUE, eval=FALSE---------------------------------
+#  # Isolate our fst matrix and locality information
+#  Fst <- Fst_dat[[1]]
+#  Loc <- Fst_dat[[2]]
+#  # Plot the heatmap, the statistic argument is used to label the plot.
+#  Fstat_hmap <- Pairwise_heatmap(dat = Fst, statistic = 'FST')
+#  # Look at the plot
+#  Fstat_hmap
 
-## ----Diversity----------------------------------------------------------------
-# Similar to our heat map, we use the statistic argument to label our figures and any output raster you create.
-Div_map <- Div_Stats_Map(dat = Het_dat, plot.type = 'point',
-statistic = "Heterozygosity", col = c('blue', 'orange', 'red'), Lat_buffer = 1, Long_buffer = 1, prefix = 'Test_het')
+## ----out.width= "600px", out.height= "350px", echo=FALSE, eval=TRUE-----------
+knitr::include_graphics("./img/Differentiation-1.png")
 
-Div_map$`Heterozygosity Map`
+## ----Differentiation map, echo = TRUE, eval=FALSE-----------------------------
+#  # Closest Neighbor
+#  Fst_map <- Dif_Stats_Map(dat = Fst, pops = Loc, neighbors = 1,
+#                       col = c('#FFFF00','#FFC000','#FFA500','#e31a1c','#800026'),
+#                       Lat_buffer = 1, Long_buffer = 1)
+#  
+#  Fst_map2 <- Dif_Stats_Map(dat = Fst, pops = Loc, neighbors = c('East_West', 'East_South', 'South_West'),
+#                       col = c('#FFFF00','#FFC000','#FFA500','#e31a1c','#800026'),
+#                       Lat_buffer = 1, Long_buffer = 1)
+#  
+#  Fst_map$`Differentiation Map`
+
+## ----out.width= "600px", out.height= "350px", echo=FALSE, eval=TRUE-----------
+knitr::include_graphics("./img/Differentiationmap-1.png")
+
+## ----echo = TRUE, eval = FALSE------------------------------------------------
+#  Fst_map2$`Differentiation Map`
+
+## ----out.width= "600px", out.height= "350px", echo=FALSE, eval=TRUE-----------
+knitr::include_graphics("./img/Differentiationmap-2.png")
+
+## ----Diversity, echo = TRUE, eval=FALSE---------------------------------------
+#  # Similar to our heat map, we use the statistic argument to label our figures and any output raster you create.
+#  Div_map <- Div_Stats_Map(dat = Het_dat, plot.type = 'point',
+#  statistic = "Heterozygosity", col = c('blue', 'orange', 'red'), Lat_buffer = 1, Long_buffer = 1, prefix = 'Test_het')
+#  
+#  Div_map$`Heterozygosity Map`
+
+## ----out.width= "600px", out.height= "350px", echo=FALSE, eval=TRUE-----------
+knitr::include_graphics("./img/Diversity-1.png")
 
